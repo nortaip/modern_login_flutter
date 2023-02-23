55c3544dee7758abe2853760a7119928347dffb7
Anar95 <anarabbas458@gmail.com>
2023-02-23 21:58:43 +0300

    Creat

---
+++ modern_login_ui_flutter/lib/page/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget buid(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        child: Center(
+          child: Column(
+           children: [
+          Icon(Icons.lock,
+          size: 100,)
+    
+
+
+
+
+
+
+      ]),
+    );
+  }
+}
