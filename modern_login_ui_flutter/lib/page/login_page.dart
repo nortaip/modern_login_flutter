5e0caf42a980bf58941898ef9854bf6d7faafc9c
Anar95 <anarabbas458@gmail.com>
2023-02-25 02:01:20 +0300

    İnputBorder

--- modern_login_ui_flutter/lib/page/login_page.dart
+++ modern_login_ui_flutter/lib/page/login_page.dart
@@ -7,37 +7,56 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body:SafeArea(
+      body: SafeArea(
         child: Center(
-          child: Column(
-            children: const [
+          child: Column(children: const [
             SizedBox(height: 50),
 
             //Logo
             Icon(
               Icons.lock,
               size: 100,
-              
-              ),
+            ),
 
-              SizedBox(height: 50),
-            //Welcome back, 
-              Text('Halaldı Asıf')
+           SizedBox(height: 50),
+
+            //Welcome back,
+            Text(
+              'Tekrar hoşgeldin, özlendin reis!',
+            style: TextStyle(
+              color: Colors.grey[700],
+              fontSize: 16,
+              ),
+            ),
+            
+             SizedBox(height: 25),
+
+            //Username textfield
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                 focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color:Colors.grey.shade400),
+                ),
+              ),
+            ),
+
             //Password textfield
-              
+
             //Forgot password
-              
+
             //Sign in button
-              
+
             //Or continue with
-              
+
             //Google + apple sign in buttons
-              
+
             //Not a member? register now
           ]),
         ),
-      ),
-    
+      )
     );
   }
 }
