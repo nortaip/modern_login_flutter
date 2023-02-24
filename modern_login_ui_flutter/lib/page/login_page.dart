33eb49907db16bd3e76a09e4049ce8c2492574bd
Anar95 <anarabbas458@gmail.com>
2023-02-25 00:50:54 +0300

    .

--- modern_login_ui_flutter/lib/page/login_page.dart
+++ modern_login_ui_flutter/lib/page/login_page.dart
@@ -7,23 +7,37 @@ class LoginPage extends StatelessWidget {
   Widget buid(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body:Column(children: [
-        //Logo
+      body:SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+            const SizedBox(height: 50),
 
-        //Welcome back, 
-
-        //Password textfield
-
-        //Forgot password
-
-        //Sign in button
-
-        //Or continue with
-
-        //Google + apple sign in buttons
-
-        //Not a member? register now
-      ]),
+            //Logo
+            const Icon(
+              Icons.lock,
+              size: 100,
+              
+              
+              
+              
+              ),
+            //Welcome back, 
+              
+            //Password textfield
+              
+            //Forgot password
+              
+            //Sign in button
+              
+            //Or continue with
+              
+            //Google + apple sign in buttons
+              
+            //Not a member? register now
+          ]),
+        ),
+      ),
     
     );
   }
