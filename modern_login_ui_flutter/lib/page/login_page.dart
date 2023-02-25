ed3d9c22c8573e3faf15af0171eca5f9c4edd645
Anar95 <anarabbas458@gmail.com>
2023-02-26 00:16:02 +0300

    Square_title

--- modern_login_ui_flutter/lib/page/login_page.dart
+++ modern_login_ui_flutter/lib/page/login_page.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:modern_login_ui_flutter/components/my_button.dart';
+import 'package:modern_login_ui_flutter/components/square_tile.dart';
 
 import '../components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-   LoginPage({super.key});
+  LoginPage({super.key});
 
   //Text editing controller
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+
+  // Sign user in metod
+
+  void signUserIn() {}
 
   @override
   Widget build(BuildContext context) {
@@ -68,19 +74,60 @@ class LoginPage extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
-              
+              ),
+
               const SizedBox(height: 25),
 
               //Sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+              const SizedBox(height: 50),
 
               //Or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                     thickness: 1,
+                     color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Aşağıdakılar ile devam edin',
+                        style: TextStyle(color: Colors.grey[700]),
+                        ),
+                    ),
+                     Expanded(
+                      child: Divider(
+                     thickness: 1,
+                     color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
               //Google + apple sign in buttons
+              Row(
+                children: [
+                  //google button
+                  SquareTile(imagePath: 'lib/logo/google.png'),
 
-              //Not a member? register now
-            ]),
+
+                  //apple button
+                   SquareTile(imagePath: 'lib/logo/apple.png'),
+                ],
+                  )
+                ],
+              ),
+            ),
           ),
-        ));
-  }
+        );
+      
+   }
 }
