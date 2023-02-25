0b5027fa6f6ead4ff8915a65a839465312091a95
Anar95 <anarabbas458@gmail.com>
2023-02-25 17:20:12 +0300

    Creat  MyTextfield

--- modern_login_ui_flutter/lib/page/login_page.dart
+++ modern_login_ui_flutter/lib/page/login_page.dart
@@ -1,63 +1,86 @@
 import 'package:flutter/material.dart';
 
+import '../components/my_textfield.dart';
+
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+   LoginPage({super.key});
+
+  //Text editing controller
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children:  [
-           const SizedBox(height: 50),
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Center(
+            child: Column(children: [
+              const SizedBox(height: 50),
 
-            //Logo
-           const Icon(
-              Icons.lock,
-              size: 100,
-            ),
-
-           const SizedBox(height: 50),
-
-            //Welcome back,
-            Text(
-              'Tekrar hoşgeldin, özlendin reis!',
-            style: TextStyle(
-              color: Colors.grey[700],
-              fontSize: 16,
+              //Logo
+              const Icon(
+                Icons.lock,
+                size: 100,
               ),
-            ),
-            
-             SizedBox(height: 25),
 
-            //Username textfield
-            TextField(
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color:Colors.grey.shade400),
+              const SizedBox(height: 50),
+
+              //Welcome back,
+              Text(
+                'Tekrar hoşgeldin, özlendin reis!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 18,
                 ),
               ),
-            ),
 
-            //Password textfield
+              const SizedBox(height: 25),
 
-            //Forgot password
+              //Username textfield
 
-            //Sign in button
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Kullanıcı Adı Giriniz',
+                obscureText: false,
+              ),
 
-            //Or continue with
+              const SizedBox(height: 15),
 
-            //Google + apple sign in buttons
+              //Password textfield
 
-            //Not a member? register now
-          ]),
-        ),
-      )
-    );
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Şifre Giriniz',
+                obscureText: true,
+              ),
+              const SizedBox(height: 15),
+
+              //Forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Şifreyimi Unutdun?',
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+              )
+              
+              const SizedBox(height: 25),
+
+              //Sign in button
+
+              //Or continue with
+
+              //Google + apple sign in buttons
+
+              //Not a member? register now
+            ]),
+          ),
+        ));
   }
 }
